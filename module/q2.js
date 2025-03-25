efaac566cf8fd6db8a6380caf2393f03e7ff3028
Shubh <91457900+Shubham0202@users.noreply.github.com>
2025-03-25 11:17:10 +0530

    update

--- module/q2.js
+++ module/q2.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const arithmetic = require('./module');
 http.createServer((req,res)=>{
-	res.end("Hey");
+	res.end("Hey  check Terminal");
 const num1 = 10;
 const num2 = 5;
 
@@ -9,4 +9,6 @@ console.log(`Addition: ${arithmetic.add(num1, num2)}`);
 console.log(`Subtraction: ${arithmetic.subtract(num1, num2)}`);
 console.log(`Multiplication: ${arithmetic.multiply(num1, num2)}`);
 console.log(`Division: ${arithmetic.divide(num1, num2)}`);
-}).listen(3000)
+}).listen(3000,()=>{
+	console.log("server is running on http://localhost:3000");
+})
